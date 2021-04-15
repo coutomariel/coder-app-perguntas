a9a33b8c1e2ef6c7e66246bbdab86d9877f50fbd
coutomariel <coutomariel@gmail.com>
2021-04-15 16:36:57 -0300

    Organização do app com widgets em um column

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,34 @@ import 'package:flutter/material.dart';
 void main() => runApp(new PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
+  final perguntas = {
+    'Qual o seu time favorito?'
+        'Qual o seu animal favorito'
+  };
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Perguntas"),
+        ),
+        body: Column(
+          children: [
+            Text(perguntas.elementAt(0)),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Alternativa 1'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Alternativa 1'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Alternativa 1'),
+            ),
+          ],
         ),
       ),
     );
